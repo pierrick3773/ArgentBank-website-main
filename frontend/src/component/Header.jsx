6613a699425424d9fb3144a13d6673c9841a6bc7
Pierrick_Viarde <128610983+pierrick3773@users.noreply.github.com>
2024-05-06 09:53:15 +0200

    signout fonctionnel

--- frontend/src/component/Header.jsx
+++ frontend/src/component/Header.jsx
@@ -1,10 +1,15 @@
 import argentBankLogo from "../assets/img/argentBankLogo.webp";
 import { NavLink } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from "../services/reducer";
 
 export function Header() {
-  const userDetails = useSelector((state) => state.auth.userDetails);
-  console.log(userDetails);
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+  const userDetails = useSelector(state => state.auth.userDetails);
+  const dispatch = useDispatch();
+  const signOut = () => {
+    dispatch(logout());
+  };
   return (
     <header>
       <nav className="main-nav">
@@ -17,15 +22,25 @@ export function Header() {
           <h1 className="sr-only">Argent Bank</h1>
         </NavLink>
         <div className="userBlock">
-        <p className="userName">{userDetails?.payload[0].userName}</p>
-          
-          <NavLink className="main-nav-item" to="/SignIn">
-            <i className="fa fa-user-circle"></i>
-            Sign In
-          </NavLink>
+          {isLoggedIn && (
+            <>
+              <p className="userName">{userDetails?.payload[0].userName}</p>
+              <NavLink className="main-nav-item-sign-out" to="/" onClick={signOut}>
+                <i className="fa fa-user-circle"></i>
+                Sign Out
+              </NavLink>
+            </>
+          )}
+          {!isLoggedIn && (
+            <NavLink className="main-nav-item" to="/SignIn">
+              <i className="fa fa-user-circle"></i>
+              Sign In
+            </NavLink>
+          )}
         </div>
       </nav>
     </header>
   );
 }
+
 
